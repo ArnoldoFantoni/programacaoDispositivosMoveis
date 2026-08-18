92a1fbfc58f1801cf145bf974b8c4d13381bd361
arnoldo <arnoldofantoni>
2026-08-17 21:06:39 -0300

    aplicado o contador

--- app/src/main/java/com/ifsc/app_aula1/MainActivity.java
+++ app/src/main/java/com/ifsc/app_aula1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ifsc.app_aula1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener( view -> {
+            //textView.setText("olaaaa");
+            contador++;
+            textView.setText(Integer.toString(contador));
+
         });
     }
 }
